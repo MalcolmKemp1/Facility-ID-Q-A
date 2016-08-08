e24c426daa6a9308ee09e21f580817509f0eb2bf
MalcolmKemp1 <malcolmkemp2000@outlook.com>
2016-08-08 10:09:54 -0400

    Update script

--- FacilityIDCleanup QA.py
+++ FacilityIDCleanup QA.py
@@ -1,21 +1,37 @@
-# CountFeatures is a function for taking the (Feature Class, Feature Layer, WHERE_CLAUSE )
-
+# CountFeatures is a function for taking the (Feature Class, Feature Layer, WHERE_CLAUSE )   
 import arcpy
 # Setting the workspace
 arcpy.env.workspace = 'Z:\FacilityIDCleanup.gdb'
 # FC = Feature Class, LYR = Feature Layer
 def CountFeatures(FC,LYR,PREFIX):
-    arcpy.MakeFeatureLayer_management(FC,LYR,"FACILITYID  LIKE '{}%'".format(PREFIX))
-    arcpy.MakeFeatureLayer_management(FC,LYR+"_","FACILITYID NOT  LIKE '{}%'".format(PREFIX))
+    arcpy.MakeFeatureLayer_management(FC,LYR,"FACILITYID LIKE '{}%'".format(PREFIX))
+    arcpy.MakeFeatureLayer_management(FC,LYR+"_","FACILITYID NOT LIKE '{}%'".format(PREFIX))
+    arcpy.MakeFeatureLayer_management(FC,LYR+"_1","FACILITYID IS NULL".format(PREFIX))
     count = arcpy.GetCount_management(LYR)
     count1 = arcpy.GetCount_management(LYR+"_")
+    count2 = arcpy.GetCount_management(LYR+"_1")
     print("The correct count for {0} is {1}".format(FC,count))
-    print("{0} of features in {1} are incorrect".format(count1,FC))
-     
+    print("{0} of features in {1} are incorrect and {2} that is NULL".format(count1,FC,count2))
+    return("The correct count for {0} is {1}".format(FC,count)) + '\r\n' + ("{0} of features in {1} are incorrect and {2} that is NULL".format(count1,FC,count2))
+    
 
-CountFeatures("wLateralLine","HYDRANNNNNNNNNNTTTTT","WLAT")
-CountFeatures("wControlValve","JOE","WCV")
 
+
+# Writing the output data as a txt file
+# 'r\n\' is for making a new line in Windows 
+
+
+
+
+#!/usr/bin/python
+
+# Open a file
+fo = open("CountFeatureClass.txt", "wb")
+#Do your business
+fo.write(CountFeatures("wLateralLine","ITLS","WLAT"));
+
+# Close opend file
+fo.close()
 
 
           
